0be1d8ac0296153c12ed66d31bf15d4a5ec20bcf
bahatisteven <bahatisteven92@gmail.com>
2025-05-14 19:03:32 +0200

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,40 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import axios from "axios";
+
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
+// fetch all vault items 
+const fetchVaultItems = async () => {
+  const response = await axios.get(`${REACT_APP_API_BASE_URL}/items`);
+  return response.data;
+};
+
+
+// fetch a single vault item
+const fetchVaultItem = async (id) => {
+  const response = await axios.get(`${REACT_APP_API_BASE_URL}/items/${id}`);
+  return response.data;
+};
+
+
+// add a new vault item
+const addVaultItem = async (item) => {
+  const response = await axios.post(`${REACT_APP_API_BASE_URL}/items`, item);
+  return response.data;
+};
+
+
+// delete a vault item 
+const deleteVaultItem = async (id) => {
+  const response = await axios.delete(`${REACT_APP_API_BASE_URL}/items/${id}`);
+  return response.data;
+};
+
+
+// fetch metrics
+const getMetrics = async () => {
+  const response = await axios.get(`${REACT_APP_METRICS_URL}`);
+  return response.data;
+};
